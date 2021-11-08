d38597349e337f3b6b59b602d4971b76f326bb0b
Matias Lasia <matiaslasia@gmail.com>
2021-11-07 23:52:03 -0300

    se avanza con tabla de usuarios

---
+++ Bloque 7/Tabla Users/Backend/server.php
@@ -0,0 +1,16 @@
+<?php
+
+$response = 
+[
+    ["ID", "USERNAME", "PASSWORD"],
+    [1, "Root", "123456"],
+    [2, "Juan", "re78934"],
+    [3, "Lorena", "******"],
+    [4, "Ariel", "aka8932j"],
+    [5, "Florencia", "jhsqnf6"],
+    [6, "Nicolas", "99os9si"]
+];
+
+echo json_encode($response);
+
+?> 
---
+++ Bloque 7/Tabla Users/main.js
@@ -0,0 +1,94 @@
+let showUserTable = ( userTable ) =>
+{
+    let HTMLCode = `<table class= "w3-table-all w3-centered fuente1 negrita" style="width:40%">`
+
+    for ( let row=0; row<userTable.length; row++)
+    {
+        HTMLCode += `<tr>`;
+
+        for (let column=0; column<userTable[row].length; column++)
+        {
+            HTMLCode += `<td>${userTable[row][column]}</td>`;
+        }
+
+        if ( row == 0 )
+        {
+            HTMLCode += `<td>ACTIONS</td>`;
+        }
+        else
+        {
+            HTMLCode += `<td>
+                        <button id='editUser-${userTable[row][0]}'class="w3-button w3-round-xlarge w3-teal" >Editar</button>
+                        <button id='deleteUser-${userTable[row][0]}'class="w3-button w3-round-xlarge w3-black" >Borrar</button>
+                    </td>`;
+        }
+
+
+        HTMLCode += `</tr>` ;                                   
+    }                      
+                        
+    HTMLCode += `</table>`;
+    HTMLCode += `<button class="w3-button w3-round-xlarge w3-orange">Nuevo Usuario</button>`;
+    return HTMLCode;
+}
+
+let onEditUserButtonClick = (event) =>
+{
+    alert('Iniciando edición del usuario...');
+}
+
+let onDeleteUserButtonClick = (event) =>
+{
+    alert('Borrando usuario...');
+}
+
+let processUsersResponse = ( event ) =>
+{
+    if ( event.currentTarget.status == 200 )
+    {
+        //1
+        let serverResponse = event.currentTarget.responseText;
+        data = JSON.parse(serverResponse);
+
+        //2
+        let userTable = document.getElementById("userTable");
+        userTable.innerHTML = showUserTable( data );
+
+        //3
+        for( let row=1; row<data.length; row++)
+        {
+            document.getElementById('editUser-'+data[row][0] ).addEventListener('click', onEditUserButtonClick );
+            document.getElementById('deleteUser-'+data[row][0] ).addEventListener('click', onDeleteUserButtonClick );
+        }
+    }
+    else
+    {
+        alert("Hubo errores al procesar la solicitud.");
+    }
+}
+
+let requestUsers = ( event ) =>
+{
+    let connection = new XMLHttpRequest();
+
+    connection.open('GET', './backend/server.php');
+
+    connection.addEventListener('loadend', processUsersResponse );
+    connection.send();
+}
+
+let initializeView = () =>
+{
+    /*1. Realizar las consultas y/o peticiones al servidor que sean necesarias
+    Para poder presentar la informaciòn inicial en la vista.*/
+    requestUsers();
+
+    /*2. Invocar las funciones que dibujan la interfaz gráfica correspondiente a cada región*/
+
+
+    /*3. Preparar toda la asociación de eventos de interacción entre el usuario y la interfaz*/
+
+}
+
+
+window.addEventListener('DOMContentLoaded', initializeView );
